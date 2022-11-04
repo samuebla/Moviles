58781f6120ddacfc85c1cd4a895c023ffd46f21d
mihern07 <mihern07@ucm.es>
2022-11-04 13:36:06 +0100

    Cambiado engine y render de PC para que no den dolores de cabeza al leerlos

--- Practica1/EngineDesktop/src/main/java/com/example/enginedesktop/RenderDesktop.java
+++ Practica1/EngineDesktop/src/main/java/com/example/enginedesktop/RenderDesktop.java
@@ -4,11 +4,17 @@ import com.example.lib.*;
 import  com.example.lib.IGraphics.*;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
+
+class WindowSize{
+    public double w;
+    public double h;
+};
 
 //Clase interna encargada de obtener el SurfaceHolder y pintar con el canvas
 public class RenderDesktop implements IGraphics {
@@ -17,26 +23,61 @@ public class RenderDesktop implements IGraphics {
     private BufferStrategy bufferStrategy;
     private Graphics2D graphics2D;
 
+    private Insets insets;
+
+    WindowSize baseSize;
+    WindowSize windowSize;
+    WindowSize nextWindowSize;
+    WindowSize nextScale;
+    WindowSize prevScale;
+
     //AAAA ESTO NO TENDRIA QUE ESTAR CREO YO
     private Scene scene;
 
     public RenderDesktop(JFrame myView){
         this.myView = myView;
-        this.myView.addComponentListener(new ComponentAdapter()
-        {
-            public void componentResized(ComponentEvent evt) {
-                //Component c = (Component)evt.getSource();
-                System.out.println("componentResized: "+evt.getSource());
-                graphics2D.dispose();
 
+        this.bufferStrategy = this.myView.getBufferStrategy();
+        this.graphics2D = (Graphics2D)bufferStrategy.getDrawGraphics();
+
+        this.insets = this.myView.getInsets();
+        this.myView.setSize(this.myView.getWidth() + this.insets.right + this.insets.left,
+                this.myView.getHeight() + this.insets.bottom + this.insets.top);
+        this.graphics2D.translate(this.insets.top, this.insets.left);
+
+        this.windowSize = new WindowSize();
+        this.windowSize.w = this.myView.getSize().width;
+        this.windowSize.h = this.myView.getSize().height;
+
+        this.nextWindowSize = new WindowSize();
+        this.nextWindowSize.w = this.myView.getSize().width;
+        this.nextWindowSize.h = this.myView.getSize().height;
+
+        this.nextScale = new WindowSize();
+        this.nextScale.w = 1.0;
+        this.nextScale.h = 1.0;
+
+        this.prevScale = new WindowSize();
+        this.prevScale.w = 1.0;
+        this.prevScale.h = 1.0;
+
+        this.myView.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                graphics2D.dispose();
                 bufferStrategy.show();
                 graphics2D = (Graphics2D) bufferStrategy.getDrawGraphics();
-                setResolution(evt.getComponent().getWidth(), evt.getComponent().getHeight());
+                double w = e.getComponent().getWidth(); double h = e.getComponent().getHeight();
+                nextScale.w = w/ windowSize.w;
+                nextScale.h = h/ windowSize.h;
+                nextWindowSize.w = w;
+                nextWindowSize.h = h;
             }
         });
 
-        this.bufferStrategy = this.myView.getBufferStrategy();
-        this.graphics2D = (Graphics2D) bufferStrategy.getDrawGraphics();
+        baseSize = new WindowSize();
+        baseSize.h = this.myView.getHeight();
+        baseSize.w = this.myView.getWidth();
+
         this.myView.setResizable(true);
     }
 
@@ -85,6 +126,14 @@ public class RenderDesktop implements IGraphics {
 //        this.graphics2D.setPaintMode();
 //    }
 
+    public void initFrame(){
+        this.graphics2D = (Graphics2D) this.bufferStrategy.getDrawGraphics();
+        this.graphics2D.setColor(Color.white);
+        this.graphics2D.fillRect(0,0, getWidth(), getHeight());
+
+        this.graphics2D.scale(this.prevScale.w, this.prevScale.h);
+    }
+
     //CREO QUE ESTA MIERDA NO TIENE QUE ESTAR AQUI NONONO
     protected void render() {
         // "Borramos" el fondo.
@@ -92,6 +141,25 @@ public class RenderDesktop implements IGraphics {
         this.graphics2D.fillRect(0,0, this.getWidth(), this.getHeight());
         // Pintamos la escena
         this.scene.render();
+    }
+
+    public void clearFrame(){
+        this.graphics2D.scale(this.nextScale.w, this.nextScale.h);
+//        this.myView.setSize((int)nextWindowSize.w, (int)nextWindowSize.h);
+        this.prevScale.w = this.nextScale.w;
+        this.prevScale.h = this.nextScale.h;
+        this.windowSize.w = (double)this.myView.getSize().width;
+        this.windowSize.h = (double)this.myView.getSize().height;
+
+        this.bufferStrategy.getDrawGraphics().dispose();
+    }
+
+    public boolean swap(){
+        if (this.bufferStrategy.contentsRestored())
+            return false;
+
+        this.bufferStrategy.show();
+        return this.bufferStrategy.contentsLost();
     }
 
     //CREO QUE ESTO TAMPOCO TIENE QUE ESTAR AYAYAY
@@ -164,10 +232,50 @@ public class RenderDesktop implements IGraphics {
     }
 
     @Override
-    public void drawText(int x,int y,String text) {
+    public void drawText(String text, int x, int y, String color){
+        Color c;
+        if(color == "red"){
+            c = Color.red;
+        } else{
+            c = Color.black;
+        }
+        this.graphics2D.setColor(c);
+        this.graphics2D.drawString(text, x, y);
+        //No estoy muy seguro de este metodo, quitar si no funciona bien
+        this.graphics2D.setPaintMode();
+    }
 
-        //this.graphics2D.setFont();
+    //EMPTY(gray) = 0
+    //SELECTED(blue) = 1
+    //CROSSED(crossed) = 2
+    //WRONG(red) = 3
+    //blank = marco vacio para la interfaz = -1
+    public void paintCell(int x, int y, int w, int h, int celltype){
+        Color c;
+        if(celltype == 1){
+            c = Color.blue;
+        } else if(celltype == 3){
+            c = Color.red;
+        } else if(celltype == 0){
+            c = Color.gray;
+        }
+        //none and blank
+        else{
+            c = Color.black;
+        }
+        this.graphics2D.setColor(c);
 
+        if (celltype== -1 || celltype == 2){
+            this.graphics2D.drawRect(x, y, w, h);
+            //Cuadrado de la interfaz
+            if (celltype == 2){
+                this.graphics2D.drawLine(x,y,w+x,h+y);
+            }
+        }else{
+            //Cambiar para que tenga en cuenta las dimensiones de la ventana, los últimos dos valores son el ancho y alto
+            this.graphics2D.fillRect(x, y, w, h);
+        }
+        this.graphics2D.setPaintMode();
     }
 }
 
--- Practica1/Interfaces(Engine)/src/main/java/com/example/lib/IGraphics.java
+++ Practica1/Interfaces(Engine)/src/main/java/com/example/lib/IGraphics.java
@@ -11,7 +11,7 @@ public interface IGraphics {
     public void drawRectangle(int x,int y,int w,int h);
     public void fillRectangle(int x,int y,int w,int h);
     public void drawLine(int x,int y,int w,int h);
-    public void drawText(int x,int y,String text);
+    public void drawText(String text, int x, int y, String color);
 
     //Other functions
     //Sacamos de aqui el ancho y alto de la pantalla???
