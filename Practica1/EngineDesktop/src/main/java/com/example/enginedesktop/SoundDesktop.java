843212171ff3916d3e950ebae41b3952a47d81d8
Samuebla <Samuebla@ucm.es>
2022-11-05 19:44:25 +0100

    MierdiCosasDeSonido

--- Practica1/EngineDesktop/src/main/java/com/example/enginedesktop/SoundDesktop.java
+++ Practica1/EngineDesktop/src/main/java/com/example/enginedesktop/SoundDesktop.java
@@ -7,22 +7,27 @@ import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundDesktop implements ISound {
 
     AudioInputStream sound;
 
+    Clip song;
+
     SoundDesktop(File file) {
         try{
-            sound = AudioSystem.getAudioInputStream(file);
+            this.sound = AudioSystem.getAudioInputStream(file);
+            this.song = AudioSystem.getClip();
+            this.song.open(sound);
         }
-        catch (IOException | UnsupportedAudioFileException e){
+        catch (IOException | UnsupportedAudioFileException | LineUnavailableException e){
             //AAA TODO QUITAR
             System.out.println("NO PUEDO CARGAR SONIDO");
             e.printStackTrace();
         }
-
     }
 
     //TODO CREO QUE FALTA UN GETTER DE AUDIOINPUTSTREAM
@@ -31,11 +36,19 @@ public class SoundDesktop implements ISound {
 
     @Override
     public void play() {
+
+        this.song.setFramePosition(0);
+        this.song.start();
     }
 
     @Override
     public void stop() {
 
+    }
+
+    @Override
+    public void startLoop() {
+        this.song.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
 }
--- Practica1/Interfaces(Engine)/src/main/java/com/example/lib/IAudio.java
+++ Practica1/Interfaces(Engine)/src/main/java/com/example/lib/IAudio.java
@@ -3,6 +3,7 @@ package com.example.lib;
 public interface IAudio{
     //Main function
     public ISound newSound();
+    public void playSound(String audioName);
 
     //Other functions
 }
