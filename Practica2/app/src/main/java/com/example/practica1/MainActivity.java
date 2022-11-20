47e4d3c32954c90b3b7bf1ffd9675a9178d74a90
mihern07 <mihern07@ucm.es>
2022-11-20 14:24:36 +0100

    Inicio de los banners de anuncios, aun queda muchisimo por hacer

--- Practica2/app/src/main/java/com/example/practica1/MainActivity.java
+++ Practica2/app/src/main/java/com/example/practica1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.practica1;
 
 import com.example.engineandroid.EngineApp;
 import com.example.engineandroid.SceneMngrAndroid;
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.AdSize;
+//import com.google.android.gms.ads.MobileAds;
+//import com.google.android.gms.ads.initialization.InitializationStatus;
+//import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.view.WindowMetrics;
+import android.widget.FrameLayout;
 
 import java.io.File;
 
@@ -21,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SceneMngrAndroid sceneMngr;
 
+    private FrameLayout adContainerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         //Creamos el SurfaceView que "contendrá" nuestra escena
         this.renderView = new SurfaceView(this);
         setContentView(this.renderView);
+
+        // Initialize the Mobile Ads SDK.
+//        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+//            @Override
+//            public void onInitializationComplete(InitializationStatus initializationStatus) { }
+//        });
 
         getSupportActionBar().hide();
         this.engine = new EngineApp(this.renderView);
@@ -40,7 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
         this.engine.setSceneMngr(this.sceneMngr);
 
-        MainMenuScene mainMenuScene = new MainMenuScene(this.engine);
+        //AAAAAAAAAAAAAAAAAAAAA
+//        this.adContainerView = findViewById(R.id.ad_view_container);
+
+        MainMenuScene mainMenuScene = new MainMenuScene(this.engine, this.adContainerView, this.getBaseContext());
         this.engine.setScene(mainMenuScene);
         this.engine.resume();
     }
@@ -56,5 +75,32 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         this.engine.pause();
     }
+
+//    private AdRequest getAdRequest() {
+//        // Create an ad request. Check your logcat output for the hashed device ID
+//        // to get test ads on a physical device, e.g.,
+//        // "Use AdRequest.Builder.addTestDevice("ABCDE0123") to get test ads on this
+//        // device."
+//
+//        return request;
+//    }
+
+    // Determine the screen width (less decorations) to use for the ad width.
+//    private AdSize getAdSize() {
+//        WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
+//        Rect bounds = windowMetrics.getBounds();
+//
+//        float adWidthPixels = adContainerView.getWidth();
+//
+//        // If the ad hasn't been laid out, default to the full screen width.
+//        if (adWidthPixels == 0f) {
+//            adWidthPixels = bounds.width();
+//        }
+//
+//        float density = getResources().getDisplayMetrics().density;
+//        int adWidth = (int) (adWidthPixels / density);
+//
+//        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+//    }
 
 }
