7df46a64a330a9a0a742c613bc8b69380fe3f1f7
mihern07 <mihern07@ucm.es>
2022-12-09 14:27:16 +0100

    Notificaciones funcionales

--- Practica2/app/src/main/java/com/example/practica1/MainActivity.java
+++ Practica2/app/src/main/java/com/example/practica1/MainActivity.java
@@ -23,11 +23,16 @@ import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -59,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     private AtomicReference<MainActivity> mainActivity;
     private final String TAG = "MainActivity";
     private Button rewardButton;
+    private final String CHANNEL_ID = "NonogramChannelId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -170,31 +176,34 @@ public class MainActivity extends AppCompatActivity {
         //---------------------------------------------------------------------
 
         //Intent example
-        //sendIntent(0, "https://twitter.com/intent/tweet", "oh wow Prueba");
+//        sendIntent(0, "https://twitter.com/intent/tweet", "oh wow Prueba");
 
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-//        if (Build.VERSION. SDK_INT >= Build.VERSION_CODES. O) {
-//            CharSequence name = getString(R.string.channel_name) ;
-//            String description = getString(R.string.channel_description) ;
-//            int importance = NotificationManager. IMPORTANCE_DEFAULT;
-//            NotificationChannel channel = new NotificationChannel(NotificationChannel.DEFAULT_CHANNEL_ID , name, importance) ;
-//            channel.setDescription(description) ;
-//            // Register the channel with the system; you can't change the importance
-//            // or other notification behaviors after this
-//            NotificationManager notificationManager = getSystemService(NotificationManager. class);
-//            notificationManager.createNotificationChannel(channel);
-//        }
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, CHANNEL_ID)
-//                .setSmallIcon(R.drawable.notification_icon)
-//                .setContentTitle( "My notification" )
-//                .setContentText( "Much longer text that cannot fit one line..." )
-//                .setStyle( new NotificationCompat.BigTextStyle()
-//                        .bigText( "Much longer text that cannot fit one line..." ))
-//                .setPriority(NotificationCompat. PRIORITY_DEFAULT);
+        if (Build.VERSION. SDK_INT >= Build.VERSION_CODES. O) {
+            CharSequence name = getString(R.string.channel_name) ;
+            String description = getString(R.string.channel_description) ;
+            int importance = NotificationManager. IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID , name, importance) ;
+            channel.setDescription(description) ;
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager. class);
+            notificationManager.createNotificationChannel(channel);
+        }
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle( "My notification" )
+                .setContentText( "Much longer text that cannot fit one line..." )
+                .setStyle( new NotificationCompat.BigTextStyle()
+                        .bigText( "Much longer text that cannot fit one line..." ))
+                .setPriority(NotificationCompat. PRIORITY_DEFAULT);
 
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(347, builder.build());
 
 
 
